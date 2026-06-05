92b69723b1cb62fa2a535138b65c1c99a7a625d8
Prissmer <133826634+Prissmer@users.noreply.github.com>
2026-06-06 02:55:16 +0800

    Fix backend server port listener and update CORS configuration

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: { 
-    origin: "http://localhost:5173", // Make sure this matches your React port!
+    origin: "https://centralis-dashboard.onrender.com", // Make sure this matches your React port!
     methods: ["GET", "POST", "PUT", "DELETE"] 
   } 
 });
